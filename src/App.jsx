f798d6479f1144bfad158845c7e275e5ead77370
Arthur Vieira <arthur.devbr@gmail.com>
2024-10-14 14:18:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-orange-500 flex items-center justify-center">
       <Form />
     </div>
   );
